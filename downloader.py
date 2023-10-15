f8647aa95ca8ca042f900d1ccfa8addeb449e0bb
kevinOdar <kevodar@gmail.com>
2023-10-15 00:07:24 -0300

    function decomposition.

--- downloader.py
+++ downloader.py
@@ -11,6 +11,36 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 # Output directory for MP3 files
 output_directory = os.path.join(current_directory, "mp3_output")
 
+# Function to download audio from a YouTube video as MP3
+def download_audio_as_mp3(video_url, output_path, quality="128kbps"):
+    try:
+        yt = YouTube(video_url)
+        audio_stream = yt.streams.filter(only_audio=True, file_extension="mp4").first()
+        if audio_stream:
+            title = re.sub(r'[\/:*?"<>|]', '', yt.title)  # Remove invalid characters
+            mp4_filename = os.path.join(output_path, f"{title}.mp4")
+            mp3_filename = os.path.join(output_path, f"{title}.mp3")
+
+            audio_stream.download(output_path=output_path, filename=title + ".mp4")
+
+            if os.path.exists(mp4_filename) and mp4_filename.endswith(".mp4"):
+                os.rename(mp4_filename, mp3_filename)
+                print(f"Downloaded audio: {title}")
+            else:
+                print(f"Error downloading audio from {video_url}: MP4 file not found or has incorrect extension")
+        else:
+            print(f"Error downloading audio from {video_url}: No MP4 audio stream found")
+    except Exception as e:
+        print(f"Error downloading audio from {video_url}: {str(e)}")
+
+# Function to load more videos
+def load_more_videos(driver):
+    continuation_elements = driver.find_elements(By.TAG_NAME, "ytd-continuation-item-renderer")
+    if len(continuation_elements) > 0:
+        actions = ActionChains(driver)
+        actions.move_to_element(continuation_elements[0]).perform()
+        time.sleep(3)  # Wait for new videos to load
+
 def download_videos_from_channel(channel_config):
     # Selenium and Chrome Driver Configuration
     options = webdriver.ChromeOptions()
@@ -25,45 +55,11 @@ def download_videos_from_channel(channel_config):
     search_title = channel_config["search_title"]
     specific_word = channel_config["specific_word"]
 
-    # Function to download audio from a YouTube video as MP3
-    def download_audio_as_mp3(video_url, output_path, quality="128kbps"):
-        try:
-            yt = YouTube(video_url)
-            audio_stream = yt.streams.filter(only_audio=True, file_extension="mp4").first()
-            if audio_stream:
-                title = re.sub(r'[\/:*?"<>|]', '', yt.title)  # Remove invalid characters
-                mp4_filename = os.path.join(output_path, f"{title}.mp4")
-                mp3_filename = os.path.join(output_path, f"{title}.mp3")
-
-                audio_stream.download(output_path=output_path, filename=title + ".mp4")
-
-                if os.path.exists(mp4_filename) and mp4_filename.endswith(".mp4"):
-                    os.rename(mp4_filename, mp3_filename)
-                    print(f"Downloaded audio: {title}")
-                else:
-                    print(f"Error downloading audio from {video_url}: MP4 file not found or has incorrect extension")
-            else:
-                print(f"Error downloading audio from {video_url}: No MP4 audio stream found")
-        except Exception as e:
-            print(f"Error downloading audio from {video_url}: {str(e)}")
-
     # Open the channel page
     driver.get(channel_url)
 
     # Wait for elements to load
     driver.implicitly_wait(10)
-
-    # Function to scroll to the end of the page
-    def scroll_to_end(driver):
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-    # Function to load more videos
-    def load_more_videos(driver):
-        continuation_elements = driver.find_elements(By.TAG_NAME, "ytd-continuation-item-renderer")
-        if len(continuation_elements) > 0:
-            actions = ActionChains(driver)
-            actions.move_to_element(continuation_elements[0]).perform()
-            time.sleep(3)  # Wait for new videos to load
 
     # Set to store displayed video links
     shown_video_links = set()
